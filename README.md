0184aaa7bd9b6a131b1020698eaed8dc041fd174
peterorlowsky <moritz@netcandy.de>
2018-02-15 13:04:20 +0100

    add slides for intro, basics and some parts of ruppert

--- README.md
+++ README.md
@@ -3,4 +3,18 @@ Presentation about Delaunay refinement, introducing Rupperts and Chews second Al
 
 ## [zur Präsentation](https://peterorlowsky.github.io/delaunay-refinement-presentation/)
 
+##Doc
+- Slides
+    - for vertical slides just interlace the sections in the index.html
+    '<section><section></section></section>'
+- GeoGebra Applet
+    - insert the id of your applet into ggMaterial Array in
+    `js/ggb.js`
+    - create a div like the snippet below with the same ID
+    - for more instructions read the comments inside the code
+    `<div class="ggb" id="GpjaA7B9"></div>`
+- LaTeX
+    - check https://www.mathjax.org/
+    `\( B = \sqrt{2}\)`
+
 Erstellt mit [reveal.js](revealjs.com) und [GeoGebra](geogebra.org).
---
+++ js/ggb.js
@@ -0,0 +1,28 @@
+/**
+ To display your ggbMaterial on a slide just drop the ID of it in the Array below and make sure there is a div with the same id inside a slide.
+ You can find the MaterialId in the URL, its the last part of it.
+ Algebra view of the material should be disabled, this option is only available in geogebra classic app.
+ You have to fiddle the size and position of your construction inside ggb and save it back to the cloud that it fits into the given frame, because there is no option in the api to make the content fit a given size..Sorry.
+ **/
+ggbMaterial = [
+    'D7XYwPfW',
+    'E975hBTQ',
+    'dCUjJyYC',
+    'NS2jCyfX',
+    'Jbf6D8ax',
+    'ndJWMvZK',
+    'a7zr7JDf',
+    'z5VVcUMA',
+    'QFJFVSXu',
+    'qKBrMvjR',
+    'GpjaA7B9'
+]
+ggbApps = ggbMaterial.map((matId) => {
+    return new GGBApplet({material_id: matId, borderColor:"#fff", width: 960, height: 600}, true);
+    })
+
+Reveal.addEventListener("ready", function() {
+        ggbApps.forEach((app, i) => {
+            app.inject(ggbMaterial[i]);
+        })
+})
